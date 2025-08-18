fb3e2a578d88ccf15c8f6f34d717512859d061c9
RaffayMa <muhammadraffay354@gmail.com>
2025-08-18 22:52:20 +0500

    edits added

--- ml-script.py
+++ ml-script.py
@@ -1,13 +1,29 @@
-import pandas as pd
+import pandas as pd # for data wrangling
+import matplotlib.pyplot as plt # for tree visualization 
+import kagglehub as kgh
+from sklearn.tree import DecisionTreeRegressor # DSA
+from sklearn.model_selection import train_test_split # for data point split
+from sklearn.metrics import mean_absolute_error # for model validation
+
+
 
 #*********
 # Data Wrangling  (R, Pandas-python)
 #*********
-  
- # Read & Store the CSV
- # Clean and create dataframe
- # Split data into training and validation data
- # Create the X (stores the input attribute) and Y (stores the predictive Attribute) VARs 
+
+ # Dowlaoding the Sample set
+path = kgh.dataset_download("yasserh/titanic-dataset")
+
+print("Path to dataset files:", path)
+
+df = pd.read_csv(os.path.join(path, 'train.csv'))
+print(df.head())
+
+
+# Read & Store the CSV
+# Clean and create dataframe
+# Split data into training and validation data
+# Create the X (stores the input attribute) and Y (stores the predictive Attribute) VARs 
  
 
 #********
